8b09c0e66f3343c736028f50ba77b8c5858f55b8
Thorium234 <inyangalapaul5@gmail.com>
2026-06-21 23:13:16 +0300

    added a get method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
@@ -7,4 +7,42 @@ app.get('/', function (req, res) {
 
 var server = app.listen(5000, function () {
    console.log("Express App running at http://127.0.0.1:5000/");
+})*/
+
+/*var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname,"index.html"));
 })
+
+var server = app.listen(5000, function () {
+
+   console.log("Express App running at http://127.0.0.1:5000/");
+})*/
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname,"index.html"));
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(5000, function () {
+   console.log("Express App running at http://127.0.0.1:5000/");
+})
+
