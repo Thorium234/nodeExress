991b90f45927989a7e63e5c0ab6ef47f43282c21
Thorium234 <inyangalapaul5@gmail.com>
2026-06-22 20:00:37 +0300

    added full databaseconnection

---
+++ databaseConn.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "mypassword"
+});
+con.connect(function (err) {
+   if (err) throw err;
+   console.log("Connected!");
+   con.query("CREATE DATABASE IF NOT EXISTS mydb;", function (err, result) {
+      if (err) throw err;
+         console.log("Created database successfully");
+   });
+   con.query("SHOW DATABASES;", function (err, result) {
+      if (err) throw err;
+         console.log("Showing databases\n");
+      for (var i = 0; i < result.length; i++) {
+         console.log(JSON.stringify(result[i]));
+      }
+   })
+   con.end();
+});
